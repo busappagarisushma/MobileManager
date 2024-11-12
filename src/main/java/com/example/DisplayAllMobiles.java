4f8db2d1b9c8dc24272081033170b7751ac543c9
busappagarisushma <sushmabusappagari@gmail.com>
2024-11-12 19:29:10 +0530

    adding JDBC logic

--- src/main/java/com/example/DisplayAllMobiles.java
+++ src/main/java/com/example/DisplayAllMobiles.java
@@ -1,7 +1,13 @@
 package com.example;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,7 +17,30 @@ public class DisplayAllMobiles extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		// JDBC Logic
+		Connection conn = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/servlet_mobiledb?user=root&password=root");
+
+			PreparedStatement pst = conn.prepareStatement("SELECT * FROM mobile");
+
+			ResultSet rs = pst.executeQuery();
+
+			req.setAttribute("resultSet", rs);
+			RequestDispatcher rd = req.getRequestDispatcher("DispalyAllMobiles.jsp");
+			rd.forward(req, resp);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
