bb828c727d1ce535146afdfdd9518a57e8d70464
busappagarisushma <sushmabusappagari@gmail.com>
2024-11-12 19:08:20 +0530

    writing JDBC logic

--- src/main/java/com/example/AddMobile.java
+++ src/main/java/com/example/AddMobile.java
@@ -1,6 +1,10 @@
 package com.example;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -16,5 +20,34 @@ public class AddMobile extends HttpServlet{
 		String mobileBrand = req.getParameter("mobileBrand");
 		int mobilePrice = Integer.parseInt(req.getParameter("mobilePrice"));
 
+		Connection conn = null;
+		//JDBC Logic
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			conn = DriverManager.getConnection
+					("jdbc:mysql://localhost:3306/servlet_mobiledb?user=root&password=root");
+
+			PreparedStatement pst = conn.prepareStatement("INSERT INTO mobile (mobileModel, mobileBrand, mobilePrice) VALUES (?, ?, ?)");
+			//			pst.setInt(1, mobileId);
+			pst.setString(1, mobileModel);
+			pst.setString(2, mobileBrand);
+			pst.setInt(3, mobilePrice);
+
+			pst.execute();
+
+			resp.sendRedirect("index.jsp");
+
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}finally {
+			if(conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
