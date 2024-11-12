208cec2494bb81398abeab59035c449644355e5e
busappagarisushma <sushmabusappagari@gmail.com>
2024-11-12 19:10:17 +0530

    adding end point

--- src/main/java/com/example/AddMobile.java
+++ src/main/java/com/example/AddMobile.java
@@ -7,10 +7,12 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/add-mobile")
 public class AddMobile extends HttpServlet{
 
 	@Override
