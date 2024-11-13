05383157923d401e67a67d1783dd9434a05d0f1d
busappagarisushma <sushmabusappagari@gmail.com>
2024-11-13 22:03:43 +0530

    creating method

--- src/main/java/com/example/DeleteMobileServlet.java
+++ src/main/java/com/example/DeleteMobileServlet.java
@@ -1,7 +1,16 @@
 package com.example;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class DeleteMobileServlet extends HttpServlet{
 
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
 }
