1de159cc94737562841c26d7e8c44e97f3915a14
busappagarisushma <sushmabusappagari@gmail.com>
2024-11-13 22:13:55 +0530

    changing logic

--- src/main/java/com/example/DeleteMobileServlet.java
+++ src/main/java/com/example/DeleteMobileServlet.java
@@ -7,10 +7,12 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/delete-mobile")
 public class DeleteMobileServlet extends HttpServlet{
 
 	@Override
