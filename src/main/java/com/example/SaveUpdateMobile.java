aee7f220f759ab6fb3de3d8a9f3a1cdacd1728dc
busappagarisushma <sushmabusappagari@gmail.com>
2024-11-14 21:58:51 +0530

    adding get method

--- src/main/java/com/example/SaveUpdateMobile.java
+++ src/main/java/com/example/SaveUpdateMobile.java
@@ -1,5 +1,16 @@
 package com.example;
 
-public class SaveUpdateMobile {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SaveUpdateMobile extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+	}
 }
