b5703bd680d9e6b61803f9fe7b949486a8791de6
busappagarisushma <sushmabusappagari@gmail.com>
2024-11-14 22:02:23 +0530

    adding Annotations

--- src/main/java/com/example/UpdateMobile.java
+++ src/main/java/com/example/UpdateMobile.java
@@ -9,10 +9,12 @@ import java.sql.SQLException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/update-mobile")
 public class UpdateMobile extends HttpServlet{
 
 	@Override
